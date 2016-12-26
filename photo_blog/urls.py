31da1c7b758b2cdcfb31494aaf7ce31d4415c03c
Avery Uslaner <uslaner.avery@gmail.com>
2016-12-25 19:58:24 -0700

    Adding uwsgi app to help deploy app.

--- photo_blog/urls.py
+++ photo_blog/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     url(r'^admin/', include(wagtailadmin_urls)),
     url(r'^documents/', include(wagtaildocs_urls)),
     url(r'^django-admin/', admin.site.urls),
+    url(r'^admin/uwsgi/', include('django_uwsgi.urls')),
 
     url(r'', include(wagtail_urls)),    # At the end so as not to override others
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

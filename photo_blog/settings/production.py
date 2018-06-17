cc1c6343253ae9facc1a2421418b0467d08af56b
Avery Uslaner <uslaner.avery@gmail.com>
2018-06-17 16:20:43 -0600

    Email TLS Cert Path Correction
    Correct path to cert files used by email encryption.
    
    Signed-off-by: Avery Uslaner <uslaner.avery@gmail.com>

--- photo_blog/settings/production.py
+++ photo_blog/settings/production.py
@@ -7,8 +7,8 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['.averyuslaner.com', '104.236.24.80']
 
-EMAIL_SSL_KEYFILE = '/etc/letsencrypt/live/conservationtechnologysolutions.com/privkey.pem'
-EMAIL_SSL_CERTFILE = '/etc/letsencrypt/live/conservationtechnologysolutions.com/fullchain.pem'
+EMAIL_SSL_KEYFILE = '/etc/letsencrypt/live/averyuslaner.com/privkey.pem'
+EMAIL_SSL_CERTFILE = '/etc/letsencrypt/live/averyuslaner.com/fullchain.pem'
 
 try:
     from .local import *

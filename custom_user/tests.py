96e7e2b32f0ce3ccc3fce6285953dd120ec897db
Avery Uslaner <uslaner.avery@gmail.com>
2018-06-17 20:20:58 -0600

    Starting PyTesting
    Setup PyTest for django and added a basic test to ensure created custom
    user objects exist.
    
    Signed-off-by: Avery Uslaner <uslaner.avery@gmail.com>

--- custom_user/tests.py
+++ custom_user/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+import pytest
 
-# Create your tests here.
+from django.contrib.auth import get_user_model
+
+
+class TestCustomUser(object):
+    @pytest.fixture(scope='module')
+    def django_db_setup(self, django_db_setup, django_db_blocker):
+        with django_db_blocker.unblock():
+            get_user_model().objects.create(username='a')
+        yield
+        with django_db_blocker.unblock():
+            get_user_model().objects.filter(username='a').delete()
+
+    @pytest.mark.django_db
+    def test_user_creation(self):
+        assert get_user_model().objects.count() == 1
+        assert get_user_model().objects.get(username='a').username == 'a'

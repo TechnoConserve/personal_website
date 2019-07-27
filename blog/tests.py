e27d205fda384525428d1c29010c8ea9cfb4a37a
Avery Uslaner <ctsadmin@conservationtechnologysolutions.org>
2019-07-27 16:03:18 -0600

    Fixed Password Reset Url Reversal
    Fixed bug where login template used incorrect name for password review
    template view.
    
    Added selenium test case for user login within blog app.
    
    Signed-off-by: Avery Uslaner <ctsadmin@conservationtechnologysolutions.org>

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium.webdriver.firefox.webdriver import WebDriver
 
-# Create your tests here.
+
+class BlogPageTests(StaticLiveServerTestCase):
+    fixtures = ['user-data.json']
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.selenium = WebDriver()
+        cls.selenium.implicitly_wait(10)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
+
+    def test_login(self):
+        self.selenium.get("{}{}".format(self.live_server_url, "/login/"))
+        username_input = self.selenium.find_element_by_name("username")
+        username_input.send_keys("test_admin")
+        password_input = self.selenium.find_element_by_name("password")
+        password_input.send_keys("password")
+
+        self.selenium.find_element_by_xpath("/html/body/div/div[2]/form/button[contains(text(), 'Log in')]").click()

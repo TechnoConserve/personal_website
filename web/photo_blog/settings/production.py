3d7dd884bf2f15694056ddcb8f4f31cdf0b8002d
Avery Uslaner <uslaner.avery@gmail.com>
2022-04-25 14:27:41 -0600

    Additional IP for production settings

--- web/photo_blog/settings/production.py
+++ web/photo_blog/settings/production.py
@@ -5,7 +5,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.averyuslaner.com', '178.128.177.110']
+ALLOWED_HOSTS = ['.averyuslaner.com', '178.128.177.110', '138.68.54.97']
 
 EMAIL_SSL_KEYFILE = '/etc/letsencrypt/live/averyuslaner.com/privkey.pem'
 EMAIL_SSL_CERTFILE = '/etc/letsencrypt/live/averyuslaner.com/fullchain.pem'

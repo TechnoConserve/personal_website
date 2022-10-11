1684e807a41e497d6335b2f2998ba9585399a599
Avery Uslaner <uslaner.avery@gmail.com>
2022-10-11 12:30:22 -0600

    Removed unnecessary line

--- web/photo_blog/urls.py
+++ web/photo_blog/urls.py
@@ -46,6 +46,3 @@ if settings.DEBUG:
 
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL + 'images/', document_root=os.path.join(settings.MEDIA_ROOT, 'images'))
-    urlpatterns += [
-        
-    ]

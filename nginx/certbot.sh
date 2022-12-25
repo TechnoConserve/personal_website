58cc835590dc4bc5c5883bb2ece22359a1826be9
Avery Uslaner <uslaner.avery@gmail.com>
2022-12-24 17:56:56 -0700

    Deletes certbot.sh

--- nginx/certbot.sh
+++
@@ -1,9 +0,0 @@
-#/bin/sh
-
-certbot certonly \
-    --agree-tos \
-    --non-interactive \
-    -m uslaner.avery@gmail.com \
-    --dns-digitalocean \
-    --dns-digitalocean-credentials /digitalocean.ini \
-    -d averyuslaner.com 

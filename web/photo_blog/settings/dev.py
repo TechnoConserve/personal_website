84e3d5685329343e93affa57e2ea5eeaa74007a2
Avery Uslaner <uslaner.avery@gmail.com>
2022-12-29 12:46:39 -0700

    Swap to Sqlite for testing and remove selenium tests

--- web/photo_blog/settings/dev.py
+++ web/photo_blog/settings/dev.py
@@ -10,6 +10,7 @@ ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0']
 
 hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
 INTERNAL_IPS = [ip[: ip.rfind(".")] + ".1" for ip in ips] + ["127.0.0.1", "10.0.2.2"]
+DATABASES = {'default': {'ENGINE': 'django.db.backends.sqlite3'} }
 
 EMAIL_SSL_KEYFILE = '/home/ave/PycharmProjects/averyuslaner.com/privkey.pem'
 EMAIL_SSL_CERTFILE = '/home/ave/PycharmProjects/averyuslaner.com/fullchain.pem'

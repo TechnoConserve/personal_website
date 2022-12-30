7bcbd7bd25f449422722a96f90e196d53df3b8bc
Avery Uslaner <uslaner.avery@gmail.com>
2022-12-30 09:51:24 -0700

    Django, asgiref, python upgrade
    Django upgraded from 3.1.13 to 3.2.16.
    
    Asgiref upgraded from 3.3.1 to 3.3.2
    
    Python upgraded from 3.8 to 3.10

--- web/photo_blog/settings/dev.py
+++ web/photo_blog/settings/dev.py
@@ -10,7 +10,6 @@ ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0']
 
 hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
 INTERNAL_IPS = [ip[: ip.rfind(".")] + ".1" for ip in ips] + ["127.0.0.1", "10.0.2.2"]
-DATABASES = {'default': {'ENGINE': 'django.db.backends.sqlite3'} }
 
 EMAIL_SSL_KEYFILE = '/home/ave/PycharmProjects/averyuslaner.com/privkey.pem'
 EMAIL_SSL_CERTFILE = '/home/ave/PycharmProjects/averyuslaner.com/fullchain.pem'
@@ -18,8 +17,3 @@ EMAIL_SSL_CERTFILE = '/home/ave/PycharmProjects/averyuslaner.com/fullchain.pem'
 INSTALLED_APPS += [
     'debug_toolbar'
 ]
-
-try:
-    from .local import *
-except ImportError:
-    pass

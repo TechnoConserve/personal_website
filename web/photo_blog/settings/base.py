f9f3f914c5d34489d7da503c2df528cdb594e97e
Avery Uslaner <uslaner.avery@gmail.com>
2022-12-31 10:22:12 -0700

    Project level static file refactor
    Project level static files placed in their own directory.
    
    Update path to blog_index_page.html static assets.
    
    Update base static file settings to specify static files directories and static file handlers.

--- web/photo_blog/settings/base.py
+++ web/photo_blog/settings/base.py
@@ -164,8 +164,20 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
+
+STATICFILES_DIRS = [
+    os.path.join(PROJECT_DIR, 'static'),
+]
+
+# ManifestStaticFilesStorage is recommended in production, to prevent outdated
+# Javascript / CSS assets being served from cache (e.g. after a Wagtail upgrade).
+# See https://docs.djangoproject.com/en/3.0/ref/contrib/staticfiles/#manifeststaticfilesstorage
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
